9b23f8583cdf3e623a41c96c4e77e5140cf242f5
Phillip <phillip7.et@gmail.com>
2019-10-12 04:50:39 +0700

    demo a scenario fetching domain  (clean code)

--- services/scrap/index.js
+++ services/scrap/index.js
@@ -2,27 +2,17 @@
 let log = console.log
 let scraper = require('../../scraper')
 module.exports = async function (fastify, opts) {
-  fastify.get('/scrap/:nameWhiteLabel/:typeSite', async function (request, reply) {
-    //log(request.body)
-    //log(request.query)
-    //log(request.params)
-    let nameWhiteLabel = request.params.nameWhiteLabel, 
+  fastify.get('/scrap/:name/:typeSite', async function (request, reply) {
+    let name = request.params.name, 
     typeSite = request.params.typeSite,
     skipAuthentication = true
-    log(nameWhiteLabel)
+    log(name)
     log(typeSite)
-    let siteData = await scraper.fetchWLSites(nameWhiteLabel)
+    let siteData = await scraper.fetchWLSites(name)
     let data = {
-        domains:await scraper.fetchWLDomains(nameWhiteLabel, typeSite, siteData, skipAuthentication),
-        ips:await scraper.fetchWLSiteAddrs(nameWhiteLabel, typeSite, siteData, skipAuthentication)
+        domains:await scraper.fetchWLDomains(name, typeSite, siteData, skipAuthentication),
+        ips:await scraper.fetchWLSiteAddrs(name, typeSite, siteData, skipAuthentication)
     }
     reply.send(data)
-    //log(request.headers)
-    //log(request.raw)
-    //log(request.id)
-    //log(request.ip)
-    //log(request.ips)
-    //log(request.hostname)
-    //request.log.info('some info')
   })
 }
